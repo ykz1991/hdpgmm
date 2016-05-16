9850cb4ad5ed2017da9a58f38d356a1b3a90b191
ykz1991 <kezi.yu@stonybrook.edu>
2016-05-16 14:50:48 -0400

    delete irrelevant comments

--- model_loglikelihood.py
+++ model_loglikelihood.py
@@ -32,32 +32,3 @@ def all_loglike(X, weights, dists):
         tmp += mixture_logpdf(x, weights, dists)
     return tmp
 
-'''
-gaussian_hl = pickle.load(open('./results/2_model/gaussian_dists_hl.p', 'rb'))
-gaussian_un = pickle.load(open('./results/2_model/gaussian_dists_un.p', 'rb'))
-weights_hl, dists_hl = dict2mix(gaussian_hl)
-weights_un, dists_un = dict2mix(gaussian_un)
-
-idx = np.load('idx_ctu.npy')
-feats = np.load('./features/feats.npy')
-pH = np.load('pH.npy')
-data = feats[idx, :, :]
-pH = pH[idx]
-data = data[:, :, [0, 1, 2, 3, 6]]
-unhealthy = np.where(pH < 7.15)[0]
-healthy = np.where(pH >= 7.15)[0]
-
-test_ind_un = np.random.choice(unhealthy, 10)
-test_ind_hl = np.random.choice(healthy, 10)
-unhealthy_data = data[test_ind_un]
-healthy_data = data[test_ind_hl]
-
-un_loglike_un = np.exp(np.array([all_loglike(d, weights_un, dists_un) for d in unhealthy_data]))
-un_loglike_hl = np.exp(np.array([all_loglike(d, weights_un, dists_un) for d in healthy_data]))
-hl_loglike_hl = np.exp(np.array([all_loglike(d, weights_hl, dists_hl) for d in healthy_data]))
-hl_loglike_un = np.exp(np.array([all_loglike(d, weights_hl, dists_hl) for d in unhealthy_data]))
-print 'loglikelihood for healthy data in healthy model is ', hl_loglike_hl
-print 'loglikelihood for healthy data in unhealthy model is ', un_loglike_hl
-print 'loglikelihood for unhealthy data in unhealthy model is ', un_loglike_un
-print 'loglikelihood for unhealthy data in healthy model is ', hl_loglike_un
-'''
